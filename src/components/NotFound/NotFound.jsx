83c7d0ab6cc4ac1e6219b01849ded36451ba5a57
Vladislav Umarov <vladdevs@yandex.ru>
2023-08-25 18:49:40 +0300

    fix: small fix

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import './NotFound.css';
 function NotFound() {
   const navigate = useNavigate();
   const handleGoBack = () => {
-    if (navigate.length >= 2) {
+    if (navigate.length > 2) {
       navigate(-1);
     } else {
       navigate('/');
